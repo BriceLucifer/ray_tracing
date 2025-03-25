b5282f81430a6d4e99f8cc43a3445bc5c592e6a7
BriceLucifer <2376671337@qq.com>
2025-03-26 01:15:53 +0800

    rendering surface normals on a sphere

--- src/ray.rs
+++ src/ray.rs
@@ -1,3 +1,5 @@
+use std::num;
+
 use crate::{color::Color, dot, vec3::Vec3};
 
 pub type Point3 = Vec3;
@@ -26,19 +28,27 @@ impl Ray {
 }
 
 pub fn ray_color(r: &Ray) -> Color {
-    if hit_sphere(&Point3::new_with_value(0.0, 0.0, -1.0), 0.5, r) {
-        return Color::new_with_value(1.0, 0.0, 0.0);
+    let t = hit_sphere(&Point3::new_with_value(0.0, 0.0, -1.0), 0.5, r);
+    if t > 0.0 {
+        let N = Vec3::unite_vector(r.at(t) - Vec3::new_with_value(0.0, 0.0, -1.0));
+        return 0.5 * Color::new_with_value(N.x() + 1.0, N.y() + 1.0, N.z() + 1.0);
     }
+
     let unit_direction = Vec3::unite_vector(r.direction());
     let a = 0.5 * (unit_direction.y() - 1.0);
     (1.0 - a) * Color::new_with_value(1.0, 1.0, 1.0) + a * Color::new_with_value(0.5, 0.7, 1.0)
 }
 
-pub fn hit_sphere(center: &Point3, radius: f64, r: &Ray) -> bool {
+pub fn hit_sphere(center: &Point3, radius: f64, r: &Ray) -> f64 {
     let oc = center.clone() - r.origin();
     let a = dot!(&r.direction(), &r.direction());
     let b = -2.0 * dot!(&r.direction(), &oc);
     let c = dot!(&oc, &oc) - radius * radius;
     let discriminant = b * b - 4.0 * a * c;
-    discriminant >= 0.0
+
+    if discriminant < 0.0 {
+        return -1.0;
+    } else {
+        return -b - discriminant.sqrt() / (2.0 * a);
+    }
 }
