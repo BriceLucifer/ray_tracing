cbf814ee4f5c3f2f519555d0522c5b70e7e4196d
BriceLucifer <2376671337@qq.com>
2025-04-22 04:05:06 +0800

    rewrite the camera.h listing39

--- src/camera.rs
+++ src/camera.rs
@@ -2,16 +2,47 @@ use crate::{
     color::Color,
     hittable::{HitRecord, HitTable},
     interval::Interval,
-    ray::Ray,
+    ray::{Point3, Ray},
     rtweekend::INFINITY,
     vec3::Vec3,
 };
 
-pub struct Camera {}
+pub struct Camera {
+    pub aspect_ratio: f64,
+    pub image_width: u32,
+    image_height: u32,
+    center: Point3,
+    pixel100_loc: Point3,
+    pixel_delta_u: Point3,
+    pixel_delta_v: Point3,
+}
 
 impl Camera {
     pub fn new() -> Self {
-        Camera {}
+        let aspect_ratio = 1.0;
+        let image_width = 100;
+        let image_height: u32 = if ((image_width as f64 / aspect_ratio) as u32) < 1 {
+            1 as u32
+        } else {
+            (image_width as f64 / aspect_ratio) as u32
+        };
+
+        let center = Point3::new();
+
+        // determine the viewport dimensions
+        let focal_length = 1.0;
+        let viewpoint_height = 2.0;
+        let viewpoint_width = viewpoint_height * (image_width as f64 / image_height as f64);
+
+        Camera {
+            aspect_ratio,
+            image_width,
+            image_height,
+            center,
+            pixel100_loc,
+            pixel_delta_u,
+            pixel_delta_v,
+        }
     }
 
     pub fn ray_color(&self, r: &Ray, world: &Box<dyn HitTable>) -> Color {
